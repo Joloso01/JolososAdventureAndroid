80a18ce2b3ade3df4bf77e0f325dfcfabac1436b
Joloso01 <jbermejop01@elpuig.xeill.net>
2021-05-27 22:47:59 +0200

    problema export solucionado

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,14 +1,19 @@
 package com.mygdx.game.desktop;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.JadventureMain;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Joloso's Adventure The Game");
-		config.setWindowIcon("sprites/ataqueIzquierda_2.png");
-		new Lwjgl3Application(new JadventureMain(), config);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Joloso's Adventure The Game";
+		config.addIcon("sprites/ataqueIzquierda_2.png", Files.FileType.Internal);
+//		config.setTitle("Joloso's Adventure The Game");
+//		config.setWindowIcon("sprites/ataqueIzquierda_2.png");
+		new LwjglApplication(new JadventureMain(), config);
 	}
 }
